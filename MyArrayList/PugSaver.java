b3a6cb6fcc1c2a4e6e80c1d943d4829b55b9766f
owen-fua <owenfua@gmail.com>
2026-08-27 11:16:39 -0700

    Worked on myArrayList

---
+++ MyArrayList/PugSaver.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class PugSaver {
+
+	//Moves every dog whose breed is "Pug" in the list to the back of the list
+	//All non-pugs must remain in the same relative order they were in originally
+	//and all pugs must also remain in the same relative order they were in originally
+	public static void rescuePugs(ArrayList<Dog> list) {
+	
+	}
+}

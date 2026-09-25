a1a1190fb7c0980d124883e371e1cf5202fd277d
owen-fua <owenfua@gmail.com>
2026-09-24 21:05:37 -0700

    Linked List
    remove, tostring, and more

--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -83,8 +83,19 @@ public class SinglyLinkedList<E> {
 	// Removes the first element that is equal to obj, if any.
 	// Returns true if successful; otherwise returns false.
 	public boolean remove(E obj) {
-		 
-
+		ListNode<E> node = head;
+		if (contains(obj)) {
+			while (node != tail) {
+				if (node.getValue().equals(obj)) {
+					node.setValue(node.getNext().getValue());
+					node.setNext(node.getNext().getNext());
+					nodeCount--;
+					return true;
+				}
+				node = node.getNext();
+			}
+		}
+		return false;
 	}
 
 	// Returns the i-th element.
@@ -110,17 +121,56 @@ public class SinglyLinkedList<E> {
 
 	// Inserts obj to become the i-th element. Increments the size
 	// of the list by one.
-	public void add(int i, E obj) {}
+	public void add(int i, E obj) {
+		if (i == 0) {
+			ListNode<E> addedNode = new ListNode<E>(obj);
+			addedNode.setNext(head);
+			head = addedNode;
+			nodeCount++;
+			return;
+		}
+		ListNode<E> node = head;
+		for (int j = 0; j < i - 1; j++) {
+			node = node.getNext();
+		}
+		ListNode<E> addedNode = new ListNode<E>(obj);
+		addedNode.setNext(node.getNext());
+		node.setNext(addedNode);
+		nodeCount++;
+	}
 
 	// Removes the i-th element and returns its value.
 	// Decrements the size of the list by one.
 	public E remove(int i) {
+		ListNode<E> node = head;
+		if (i == 0) {
+			head = head.getNext();
+			nodeCount--;
+			return node.getValue();
+		}
+		for (int j = 0; j < i - 1; j++) {
+			node = node.getNext();
+		}
+		ListNode<E> removedNode = node.getNext();
+		node.setNext(node.getNext().getNext());
+		nodeCount--;
+		return removedNode.getValue();
 
 	}
 
 	// Returns a string representation of this list exactly like that for MyArrayList.
 	public String toString() {
-
+		if (isEmpty()) {
+			return "[]";
+		}
+		String output = "[";
+		ListNode<E> node = head;
+		for (int i = 0; i < nodeCount - 1; i++) {
+			output += node.getValue() + ", ";
+			node = node.getNext();
+		}
+		output += tail.getValue() + "]";
+		return output;
 
 	}
 
--- LinkedList/Tester.java
+++ LinkedList/Tester.java
@@ -2,11 +2,27 @@ public class Tester {
     public static void main(String[] args) {
         Integer[] values = {1, 2, 3, 4};
         SinglyLinkedList list = new SinglyLinkedList<Integer>(values);
-
         System.out.println(list.get(0));
         System.out.println(list.get(1));
-        System.out.println(list.getHead().getValue());
-        list.set(0, 9);
+        Integer test = 1;
+        list.remove(test);
         System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        System.out.println(list.get(2));
+
+        System.out.println(" ");
+        System.out.println(list.toString());
+        
+        Integer test2 = 7;
+        list.add(1, test2);
+        
+        System.out.println(" ");
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        System.out.println(list.get(2));
+        System.out.println(list.get(3));
+
+        System.out.println(list.toString());
+
     };
 }
